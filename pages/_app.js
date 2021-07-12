fc01315b8c1b931acd660a615fb0215ecc8cf323
Sophie Kowalski <skowalski57@gmail.com>
2021-07-12 18:25:14 +0200

    Styling using styled components

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,30 @@
-import Link from 'next/link';
+import Navbar from '../components/Navbar';
 import styled from 'styled-components';
 import { Normalize } from 'styled-normalize';
 
 const Container = styled.div`
   @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Merriweather:wght@400;700&display=swap');
 
-  background: pink;
+  background: linear-gradient(to right, #c9d6ff, #e2e2e2);
   font-family: 'Lato', sans-serif;
+  color: #333;
+  min-height: 100vh;
+`;
+
+const Page = styled.div`
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
 `;
 
 function MyApp({ Component, pageProps }) {
   return (
     <Container>
       <Normalize />
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Component {...pageProps} />
+      <Navbar />
+      <Page>
+        <Component {...pageProps} />
+      </Page>
     </Container>
   );
 }
